efc8ce44ae266a127c2fedcf446c758a8f79f90e
gabrielalmir <gabr.almir@gmail.com>
2024-07-28 23:08:42 -0300

    feat: create a http server class

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+
+public class HttpServer {
+    private final int port;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        try (var serverSocket = new ServerSocket(port)) {
+            serverSocket.setReuseAddress(true);
+            while (true) { handleRequest(serverSocket); }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void handleRequest(ServerSocket serverSocket) {
+        try (var socket = serverSocket.accept()) {
+            System.out.println("Accepted new connection");
+
+            var inputStream = socket.getInputStream();
+            var reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            System.out.println(reader.readLine());
+
+            var outputStream = socket.getOutputStream();
+            outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,7 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Logs from your program will appear here!");
-
-        try (var serverSocket = new ServerSocket(4221)) {
-            serverSocket.setReuseAddress(true);
-
-            try (var socket = serverSocket.accept()) {
-                System.out.println("Accepted new connection");
-                var outputStream = socket.getOutputStream();
-                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                outputStream.flush();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("Running HTTP Server at port 4221 ...");
+        var httpServer = new HttpServer(4221);
+        httpServer.start();
     }
 }
