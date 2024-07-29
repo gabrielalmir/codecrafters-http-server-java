ef22053602c2922ee8c6b82aa82b88c83eaeaa7d
gabrielalmir <gabr.almir@gmail.com>
2024-07-28 22:54:32 -0300

    feat: respond http 200 ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,23 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
 
-     try {
-       var serverSocket = new ServerSocket(4221);
+        try (var serverSocket = new ServerSocket(4221)) {
+            serverSocket.setReuseAddress(true);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            try (var socket = serverSocket.accept()) {
+                System.out.println("Accepted new connection");
+                var outputStream = socket.getOutputStream();
+                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                outputStream.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
